204bbd406d51b7797ff23e484538516ce3b046f5
Petrik Diak <diak@petrik.hu>
2025-02-07 12:55:25 +0100

    filmes feladatok mindennel is

---
+++ oopgyakorlas/Film.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopgyakorlas
+{
+	internal class Film
+	{
+		private string cim;
+		private string rendezo;
+		private int hosszPercekben;
+		private string mufaj;
+		private bool megjelent;
+
+		public string Cim { get => cim; set => cim = value; }
+		public string Rendezo { get => rendezo; set => rendezo = value; }
+		public int HosszPercekben { get => hosszPercekben; set => hosszPercekben = value; }
+		public string Mufaj { get => mufaj; set => mufaj = value; }
+		public bool Megjelent { get => megjelent; set => megjelent = value; }
+
+		public Film(string cim, string rendezo, int hosszPercekben, string mufaj, bool megjelent)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;
+			this.hosszPercekben = hosszPercekben;
+			this.mufaj = mufaj;
+			this.megjelent = megjelent;
+		}
+
+		public Film(string cim, string rendezo)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;
+			hosszPercekben = 120;
+			mufaj = "Ismeretlen";
+			megjelent = false;
+		}
+
+		public void Jatszas()
+		{
+			Console.WriteLine($"\nA {cim} elkezdődött.");
+		}
+		public void MufajModositas(string ujMufaj)
+		{
+			Console.Write("Adja meg a módosított műfajt: ");
+			ujMufaj = Console.ReadLine();
+			mufaj = ujMufaj;
+            Console.WriteLine($"\n{cim} - {rendezo} / {hosszPercekben} / {ujMufaj} - {megjelent}");
+		}
+
+		public void HosszNovelese(int percek)
+		{
+            Console.Write("Adja meg hány perccel szeretné növelni a film hosszát: ");
+			percek = Convert.ToInt32(Console.ReadLine());
+			hosszPercekben += percek;
+			Console.WriteLine($"\n{cim} - {rendezo} / {hosszPercekben} / {mufaj} - {megjelent}");
+        }
+
+		public override string ToString()
+		{
+			return $"{cim} - {rendezo} / {hosszPercekben} / {mufaj} - {megjelent}";
+		}
+	}
+}
--- oopgyakorlas/Program.cs
+++ oopgyakorlas/Program.cs
@@ -13,6 +13,17 @@
 			konyv1.Arnoveles(1000);
 
 			Console.WriteLine("\n" + konyv1.ToString() + "\n" + konyv1.HanyoldalMaradt(20));
+
+            Console.WriteLine();
+            Film film1 = new ("Titanic", "James Cameron", 195, "romantikus", true);
+			Film film2 = new ("Ez még készül", "Én");
+			Console.WriteLine(film1.ToString());
+            Console.WriteLine();
+            film1.Jatszas();
+            Console.WriteLine();
+            film1.MufajModositas("");
+			Console.WriteLine();
+			film1.HosszNovelese(0);
 		}
 	}
 }
